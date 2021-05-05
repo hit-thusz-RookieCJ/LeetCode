ddc38f8f4e20a6348453be07c698fc2c132e9bd2
hit-thusz-RookieCJ <295951960@qq.com>
2021-05-05 17:50:22 +0800

    2021.5.5

---
+++ q740.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int rob(vector<int> &nums)
+    {
+        int size = nums.size();
+        int first = nums[0] , second = nums[1];
+        for(int i = 2; i < size; i++)
+        {
+            int temp = second;
+            second = max(second, nums[i] + first);
+            first = temp;
+        }
+        return second;
+
+    }
+    
+    int deleteAndEarn(vector<int>& nums) {
+        int maxVal = 0;
+        for(int num : nums)
+        {
+            maxVal = max(maxVal, num);
+        }
+        vector<int> sum(maxVal + 1);    //初始化后默认为0
+        for(int num : nums)
+        {
+            sum[num] += num;
+        }
+        return rob(sum);
+
+
+    }
+};
